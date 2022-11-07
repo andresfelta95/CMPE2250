c2d6efbd328b422f40793d4e2f0c1da74f151199
Andres Tangarife <andresfelta95@gmail.com>
2022-11-07 08:16:52 -0700

    New SCI Lib

---
+++ Lib/Src/sci.c
@@ -0,0 +1,11 @@
+// implementation file for DCI
+// Processor: MC9S12XDP512
+// Crystal: 16 MHz
+// by Andres Tangarife
+// November, 2022
+
+#include <hidef.h>      /* common defines and macros */
+#include "derivative.h" /* derivative-specific definitions */
+#include "math.h"
+#include "Clock.h"
+#include "ECT.h"
