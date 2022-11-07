f7beed17d42b43153d27407b07c4f0459cc43e2e
Andres Tangarife <andresfelta95@gmail.com>
2022-11-07 08:42:53 -0700

    Started Lab01

--- ICAs/ICA04/Sources/main.c
+++ ICAs/ICA04/Sources/main.c
@@ -64,16 +64,18 @@ void main(void)
     //The delay between the PIT and the timer is 146us
     //The difference between the two is that the PIT has a priority over the timer
     #endif  //PART_A
-    //#ifdef PART_B
+    #ifdef PART_B
     //asm sei; //enable interrupts
     //Timer_Sleep_ms(20); //Sleep for 20ms
     //asm cli; //disable interrupts
     // Answers:
     // The delay betweem PIT and Timer was >5 seconds.
-    // 1. The PIT should occur first and it can be proved by the fact that the PIT count is incremented first.
-    // 2. We can prove it checking waveforms from the LEDs and verifying the time between the Yellow LED and the Green LED.
-    // 3. The PIT has a higher priority than the timer.
-    //#endif  //PART_B
+    // 1. The PIT should occur first and it can be proved by the fact that 
+    //    the PIT count is incremented first.
+    // 2. We can prove it checking waveforms from the LEDs and verifying 
+    //    the time between the Yellow LED and the Green LED.
+    // 3. The reason is because of the blocking delay the timer skips some cycles and the PIT is not affected by this.
+    #endif  //PART_B
     
     ShowCount(); // display the PIT count on the 7-segment display
   }                   
@@ -95,8 +97,15 @@ void ShowCount(void)
 interrupt VectorNumber_Vtimch0 void IOC0 (void)
 {
   // interrupt service routine for channel 0
-  //TC0 += 3125; // set the next interrupt time
+  TC0 += 3125; // set the next interrupt time
+  #ifdef PART_C
   TC0 = TCNT + 3125; // set the next interrupt time
+  //  Answers:
+  //  1. The PIT keeps occuring first but the difference is smaller.
+  //     However, at the beginning the diference is smaller and then it increases.
+  //  2. If it is set in the first line it helps to keep the rearm time of the timer closer 
+  //     to the PIT.
+  #endif  //PART_C
   // take action!
   if (timCnt++ > 9999)
   {
--- Lib/Src/sci.c
+++ Lib/Src/sci.c
@@ -9,3 +9,4 @@
 #include "math.h"
 #include "Clock.h"
 #include "ECT.h"
+
